1c406f283067bf85b6d5efd2e8bb37dfab30aef2
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2015-06-30 17:54:08 +0300

    initial

---
+++ src/Code.js
@@ -0,0 +1,32 @@
+/** @jsx deku.dom */
+
+import deku from 'deku';
+import hljs from 'highlight.js';
+
+export default {
+    render({ props }) {
+        const { dataUrl, clientId } = props;
+        const htmlCode = hljs.highlight('html', `<div data-url="${dataUrl}" class="sb-soundplayer-widget"></div>
+<script>
+/* * * CONFIGURATION VARIABLES * * */
+var sb_soundplayer_client_id = '${clientId}';
+
+/* * * DON'T EDIT BELOW THIS LINE * * */
+(function(d, s, id) {
+    if (d.getElementById(id)) return;
+    var js, p, fjs = d.getElementsByTagName(s)[0];
+    p = /^http:/.test(d.location) ? 'http' : 'https';
+    js = d.createElement(s);
+    js.id = id;
+    js.src = p + '://cdnjs.cloudflare.com/ajax/libs/soundplayer-widget/0.1.3/soundplayer-widget.min.js';
+    fjs.parentNode.insertBefore(js, fjs);
+})(document, 'script', 'sb-soundplayer-widget-sdk');
+</script>`);
+
+        return (
+            <div>
+                <pre><code class="html" innerHTML={htmlCode.value}></code></pre>
+            </div>
+        );
+    }
+};
---
+++ src/app.js
@@ -0,0 +1,88 @@
+/** @jsx deku.dom */
+
+import 'babel/polyfill';
+
+import deku from 'deku';
+import SoundPlayer from 'deku-soundplayer';
+import SoundCloudAudio from 'soundcloud-audio';
+
+import Code from './Code';
+
+const App = {
+    initialState() {
+        return {
+            dataUrl: 'https://soundcloud.com/shura/shura-white-light',
+            clientId: '08f79801a998c381762ec5b15e4914d5'
+        };
+    },
+
+    render({ state }, updateState) {
+        const { dataUrl, clientId } = state;
+        let form = Object.assign({}, state);
+
+        console.log('RENDER', state);
+
+        function getTrackUrl (e) {
+            form.dataUrl = e.target.value;
+        }
+
+        function getClientId (e) {
+            form.clientId = e.target.value;
+        }
+
+        function getPlayer () {
+            console.log(form);
+            updateState(form);
+        }
+
+        return (
+            <div>
+                <div>
+                    <h2 id="Track" class="h3 caps mt4">
+                        <a href="#Track" class="black">Step 1. SoundCloud Track Url</a>
+                    </h2>
+                    <hr class="mt1 mb1 b2 border-darken-2" />
+                    <div class="mt2">
+                        <input id="track" type="text" class="field-light full-width" placeholder="https://soundcloud.com/shura/shura-indecision-12-edit-1" onChange={getTrackUrl} value={form.dataUrl} />
+                    </div>
+                </div>
+
+                <div>
+                    <h2 id="ClientId" class="h3 caps mt4">
+                        <a href="#ClientId" class="black">Step 2. SoundCloud Client ID</a>
+                    </h2>
+                    <hr class="mt1 mb1 b2 border-darken-2" />
+                    <div class="mt2">
+                        <input id="clientId" type="text" class="field-light full-width" placeholder="08f79801a998c381762ec5b15e4914d5" onChange={getClientId} value={form.clientId} />
+                    </div>
+                </div>
+
+                <div class="center mt3">
+                    <button class="button button-outline button-big border-olive olive b2 caps" onClick={getPlayer}>Get Player</button>
+                </div>
+
+                {dataUrl && clientId ? (
+                    <div>
+                        <h2 id="Player" class="h3 caps mt4">
+                            <a href="#Player" class="black">Step 3. Copy/Paste Player Code</a>
+                        </h2>
+                        <hr class="mt1 mb1 b2 border-darken-2" />
+
+                        {/* Preview */}
+                        <div class="mt2">
+                            <SoundPlayer class="wrap" url={dataUrl} soundCloudAudio={new SoundCloudAudio(clientId)} />
+                        </div>
+
+                        {/* Get Code */}
+                        <div class="mt2">
+                            <Code dataUrl={dataUrl} clientId={clientId} />
+                        </div>
+                    </div>
+                ) : <span />}
+            </div>
+        );
+    }
+};
+
+const app = deku.tree(<App />);
+deku.render(app, document.getElementById('get-soundplayer'));
